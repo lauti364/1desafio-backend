5d1942fe5cdd02724444cffb9f6549e4390de0c4
Lautaro Vazquez <lautivp16@gmail.com>
2024-06-17 14:47:41 -0300

    cartaluser

--- dao/models/usuarios.model.js
+++ dao/models/usuarios.model.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const Cart = require('./cart.model');
 
 const userSchema = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    email: { type: String, unique: true },
-    age: Number,
-    password: String,
-    role: { type: String, enum: ['admin', 'usuario'], default: 'usuario' }
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    age: { type: Number, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'usuario'], default: 'usuario', required: true },
+    cart: { type: mongoose.Schema.Types.ObjectId, ref: 'Cart' }  // Referencia al carrito
 });
 
 userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+userSchema.pre('save', async function(next) {
+    if (this.isNew) {
+        const cart = new Cart({ user: this._id, products: [] });
+        await cart.save();
+        this.cart = cart._id;
+    }
     next();
 });
 
